b02bcbdd8368471733bd65b0c5285cc7f3eef6d4
VictoriaMS <vikamag48@gmail.com>
2022-07-04 00:30:02 +0600

    Created spec for feature

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,13 @@
 FactoryBot.define do
+  sequence :email do |n|
+    "user#{n}@test.com"
+  end
+  
   factory :user do
     name { "John" }
     last_name  { "Doe" }
+    email 
+    password { "123456" }
+    password_confirmation { "123456" } 
   end
 end
